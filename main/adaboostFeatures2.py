044189002936d33069d8af672b455ce9ea3af0ea
Vincent Pham <vincentpham@gmail.com>
2016-02-25 22:10:20 -0800

    updated adaboost to include more features

--- main/adaboostFeatures2.py
+++ main/adaboostFeatures2.py
@@ -3,65 +3,65 @@ __team__ = "CaptainDataCrunch"
 
 from adaboostImages import *
 
-def feat_two_rectangles(gray_img, block_num):
+def feat_two_rectangles(gray_img, coords):
 	"""Calculates integral images for two windows A and B then finds their difference
 	:param gray_img: array of pixels
-	:param block_num: tuple (block's upper left corner, block's bottom right corner)
+	:param coords: tuple (block's upper left corner, block's bottom right corner)
 	:return: I(B) - I(A) where I() = integral image
 	"""
-	half_x = (block_num[2]-block_num[0])/2 - 1
-	A = (block_num[0], block_num[1], half_x, block_num[3])
-	B = (half_x + 1, block_num[1], block_num[2], block_num[3])
+	half_x = (coords[2] - coords[0]) / 2 - 1
+	A = (coords[0], coords[1], half_x, coords[3])
+	B = (half_x + 1, coords[1], coords[2], coords[3])
 
 	A_sum = integralImage(gray_img,A)
 	B_sum = integralImage(gray_img,B)
 	return (B_sum - A_sum)
 
-def feat_three_rectangles_horizontal(gray_img, block_num, height1=24, height2=14):
+def feat_three_rectangles_horizontal(gray_img, coords, height1=24, height2=14):
 	"""Calculates integral images for three windows A, B, and C
 	:param gray_img: array of pixels
-	:param block_num: tuple (block's upper left corner, block's bottom right corner)
+	:param coords: tuple (block's upper left corner, block's bottom right corner)
 	:return: I(B) - (I(A) + I(C)) where I() = integral image
 	"""
 
-	A = (block_num[0], block_num[1], block_num[2], block_num[13])
-	B = (block_num[0], block_num[1]+height1+1, block_num[2],  block_num[1]+height1+height2)
-	C = (block_num[0], block_num[1]+height1+height2+1, block_num[2], block_num[3])
+	A = (coords[0], coords[1], coords[2], coords[13])
+	B = (coords[0], coords[1] + height1 + 1, coords[2], coords[1] + height1 + height2)
+	C = (coords[0], coords[1] + height1 + height2 + 1, coords[2], coords[3])
 
 	A_sum = integralImage(gray_img,A)
 	B_sum = integralImage(gray_img,B)
 	C_sum = integralImage(gray_img,C)
 	return (B_sum - (A_sum + C_sum))
 
-def feat_three_rectangles_vertical(gray_img, block_num, width1=52, width2=24):
+def feat_three_rectangles_vertical(gray_img, coords, width1=52, width2=24):
 	"""Calculates integral images for three windows A, B, and C
 	:param gray_img: array of pixels
-	:param block_num: tuple (block's upper left corner, block's bottom right corner)
+	:param coords: tuple (block's upper left corner, block's bottom right corner)
 	:return: I(B) - (I(A) + I(C)) where I() = integral image
 	"""
 
-	A = (block_num[0], block_num[1], block_num[0]+width1, block_num[3])
-	B = (block_num[0]+width1+1, block_num[1], block_num[0]+width1+width2, block_num[3])
-	C = (block_num[0]+width1+width2+1, block_num[1], block_num[2], block_num[3])
+	A = (coords[0], coords[1], coords[0] + width1, coords[3])
+	B = (coords[0] + width1 + 1, coords[1], coords[0] + width1 + width2, coords[3])
+	C = (coords[0] + width1 + width2 + 1, coords[1], coords[2], coords[3])
 
 	A_sum = integralImage(gray_img,A)
 	B_sum = integralImage(gray_img,B)
 	C_sum = integralImage(gray_img,C)
 	return (B_sum - (A_sum + C_sum))
 
-def feat_four_rectangles(gray_img, block_num):
+def feat_four_rectangles(gray_img, coords):
 	"""Calculates integral images for four windows in a square [A, B][C, D]
 	:param gray_img: array of pixels
-	:param block_num: tuple (block's upper left corner, block's bottom right corner)
+	:param coords: tuple (block's upper left corner, block's bottom right corner)
 	:return: (I(A) + I(D)) - (I(B) + I(C)) where I() = integral image
 	"""
-	half_x = (block_num[2] - block_num[0])/2 - 1
-	half_y = (block_num[3] - block_num[1])/2 - 1
+	half_x = (coords[2] - coords[0]) / 2 - 1
+	half_y = (coords[3] - coords[1]) / 2 - 1
 
-	A = (block_num[0], block_num[1], half_x, half_y)
-	B = (half_x + 1, block_num[1], block_num[2], half_y)
-	C = (block_num[0], half_y + 1, half_x, block_num[3])
-	D = (half_x + 1, half_y + 1, block_num[2], block_num[3])
+	A = (coords[0], coords[1], half_x, half_y)
+	B = (half_x + 1, coords[1], coords[2], half_y)
+	C = (coords[0], half_y + 1, half_x, coords[3])
+	D = (half_x + 1, half_y + 1, coords[2], coords[3])
 
 	A_sum = integralImage(gray_img,A)
 	B_sum = integralImage(gray_img,B)
--- main/runAdaboost.py
+++ main/runAdaboost.py
@@ -5,8 +5,8 @@ import time
 from adaboostTrain import *
 from adaboostTest import *
 
-neg_filepath = '/Users/vincentpham/Desktop/tester/negative/'
-pos_filepath = '/Users/vincentpham/Desktop/tester/positive/'
+neg_filepath = '/Users/vincentpham/Desktop/tester/negative10_resized/'
+pos_filepath = '/Users/vincentpham/Desktop/tester/positive10_resized/'
 
 neg_testpath = '/Users/vincentpham/Desktop/tester/negative_test/'
 pos_testpath = '/Users/vincentpham/Desktop/tester/positive_test/'
@@ -15,6 +15,7 @@ def main():
 	start = time.time()
 	trained_model = adaboost_train(pos_filepath, neg_filepath, T = 3)
 	cp1 = time.time()
+	print "alphas:", trained_model[0]
 	print "training finished in:", ((cp1 - start)/60), "min"
 
 	test_training_images(pos_testpath, neg_testpath, trained_model)
