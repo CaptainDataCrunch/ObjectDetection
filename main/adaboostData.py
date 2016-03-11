ad0ba9db4d1fd37486a32e1e7a57d2e119ec1f3f
Vincent Pham <vincentpham@gmail.com>
2016-03-11 09:53:55 -0800

    added plots, renamed parallel folder

--- main/adaboostData.py
+++ main/adaboostData.py
@@ -4,15 +4,17 @@ __team__ = "CaptainDataCrunch"
 import glob
 import pickle
 
+
 def load_data(filepath, label_value):
-	"""Loads in image and label
-	:param filepath: path of the directory containing all files
-	:param label_value: classification label
-	:return: list of tuples(ndarray, label)
-	"""
-	files = glob.glob(filepath + '*.jpg')
-	labels = [label_value for i in range(len(files))]
-	return zip(files, labels)
+    """Loads in image and label
+    :param filepath: path of the directory containing all files
+    :param label_value: classification label
+    :return: list of tuples(ndarray, label)
+    """
+    files = glob.glob(filepath + '*.jpg')
+    labels = [label_value for i in range(len(files))]
+    return zip(files, labels)
+
 
 def save_params(alphas, best_models, best_coords, best_features, error_rate_list, error_dict):
     """Saves parameters to pickle files
@@ -22,17 +24,18 @@ def save_params(alphas, best_models, best_coords, best_features, error_rate_list
     :param best_features: list of features that classified best
     :param error_rate_list: list of error rates
     :param error_dict: dict where key = model, feature, coord and value = (error, prediction)
-    """
-	# saving parameters to files
-	pickle.dump(alphas, open("save_alphas.p", "wb"))
-	pickle.dump(best_coords, open("save_coords.p", "wb"))
-	pickle.dump(best_models, open("save_models.p", "wb"))
-	pickle.dump(best_features, open("save_features.p", "wb"))
-	pickle.dump(error_rate_list, open("save_error_rate_list.p", "wb"))
-	pickle.dump(error_dict, open("save_error_dict.p", "wb"))
-	return "Parameters saved!"
+    """  # saving parameters to files
+    pickle.dump(alphas, open("save_alphas.p", "wb"))
+    pickle.dump(best_coords, open("save_coords.p", "wb"))
+    pickle.dump(best_models, open("save_models.p", "wb"))
+    pickle.dump(best_features, open("save_features.p", "wb"))
+    pickle.dump(error_rate_list, open("save_error_rate_list.p", "wb"))
+    pickle.dump(error_dict, open("save_error_dict.p", "wb"))
+    return "Parameters saved!"
 
-def load_params(alpha_file='save_alphas.p', model_file='save_models.p', coords_file='save_coords.p', feature_file='save_features.p'):
+
+def load_params(alpha_file='save_alphas.p', model_file='save_models.p', coords_file='save_coords.p',
+                feature_file='save_features.p'):
     """Loads the pickle files containing the parameters
     :param alpha_file: pickle file containing alpha values
     :param model_file: pickle file containing best models
@@ -40,11 +43,12 @@ def load_params(alpha_file='save_alphas.p', model_file='save_models.p', coords_f
     :param feature_file: pickle file containing best features
     :return: tuple(alphas, models, coords, features)
     """
-	test_alphas = pickle.load(open(alpha_file, "rb"))
-	test_coords = pickle.load(open(coords_file, "rb"))
-	test_models = pickle.load(open(model_file, "rb"))
-	test_features = pickle.load(open(feature_file, "rb"))
-	return test_alphas, test_models, test_coords, test_features
+    test_alphas = pickle.load(open(alpha_file, "rb"))
+    test_coords = pickle.load(open(coords_file, "rb"))
+    test_models = pickle.load(open(model_file, "rb"))
+    test_features = pickle.load(open(feature_file, "rb"))
+    return test_alphas, test_models, test_coords, test_features
+
 
 def load_param(path, file):
     """Load a pickle file
@@ -52,5 +56,5 @@ def load_param(path, file):
     :param file: name of pickle file
     :return: parameter from file
     """
-	param = pickle.load(open(path + file, "rb"))
-	return param
+    param = pickle.load(open(path + file, "rb"))
+    return param
--- main/plots.py
+++ main/plots.py
@@ -1,9 +1,84 @@
 __authors__ = "Isabel Litton, Vincent Pham, Henry Tom"
 __team__ = "CaptainDataCrunch"
 
-from adaboostData import *
+from adaboostImages import *
+import numpy as np
+from ggplot import *
+import pandas as pd
 
 filepath = "/Users/vincentpham/CaptainDataCrunch-/main/final_model/"
+pos_trainpath = "/Users/vincentpham/Desktop/tester/final_pos_train/"
+neg_trainpath = "/Users/vincentpham/Desktop/tester/final_neg_train/"
+
+pos_testpath = "/Users/vincentpham/Desktop/tester/final_pos_test/"
+neg_testpath = "/Users/vincentpham/Desktop/tester/final_neg_test/"
+
+def calculate_products(gray_img, alphas, best_models, best_coords, best_features, T = None):
+    """Calculates the products of alphas * intergral image
+    :param gray_img: array of pixel values
+    :param alphas: list where the elements are weights wegihts of the models
+    :param best_models: list of models selected by adaboost
+    :param best_coords: list of blocks selected by adaboost
+    :param best_features: list of features selected by adaboost
+    :param T: number of iteration to look at
+    :return: List of products
+    """
+    if T is None:
+        T = len(alphas)
+
+    haar = [best_features[i](gray_img, best_coords[i]) for i in range(T)]
+    haar = [[x] for x in haar]
+    predictions = [best_models[i].predict(haar[i]) for i in range(T)]
+
+    products = [x[0] * x[1] for x in zip(predictions, alphas)]
+    return products
+
+
+def get_training_accuracy(pos_testpath, neg_testpath, trained_model, T = None):
+    """ test files in test directory
+    :param pos_testpath: directory of positive files
+    :param neg_testpath: directory of negative files
+    :param trained_model: trained model of (alphas, models, coords, features)
+    :param T: number of iteration to look at
+    :return:
+    """
+    images = get_gray_imgs(pos_testpath, neg_testpath)
+    if T == None:
+        T = len(trained_model[0])
+
+    #accuracy_list = list()
+    # for i in range(T):
+
+    positive_counter = [0]*T
+    negative_counter = [0]*T
+    pos_acc = [0]*T
+    neg_acc = [0]*T
+
+    print_count = 0
+    for gray_img, label in images:
+        print print_count
+        print_count += 1
+
+        products = calculate_products(gray_img, trained_model[0], trained_model[1], trained_model[2],
+                                                trained_model[3])
+        for i in range(T):
+            if label == 1:
+                positive_counter[i] += 1.0
+            elif label == -1:
+                negative_counter[i] += 1.0
+
+            prediction = np.sign(sum(products[:(i+1)]))
+            if prediction == label and label == 1:
+                pos_acc[i] += 1.0
+            if prediction == label and label == -1:
+                neg_acc[i] += 1.0
+
+        #accuracy_list.append([i, pos_acc, neg_acc, positive_counter, negative_counter])
+    d = {"T":pd.Series([i for i in range(T)]), "pos_acc_count":pd.Series(positive_counter),
+         "neg_acc_count":pd.Series(negative_counter),
+         "pos_count":pd.Series(pos_acc),"neg_count":pd.Series(neg_acc)}
+    df = pd.DataFrame(d)
+    return df
 
 alphas = load_param(filepath, "save_alphas.p")
 coords = load_param(filepath, "save_blocks.p")
@@ -18,3 +93,59 @@ print "errors", error_rate_list
 print
 print "features", features
 #print "models", models
+n = len(alphas)
+
+
+d = {"T":pd.Series([i for i in range(n)]), "error":pd.Series(error_rate_list),
+     "alpha":pd.Series(alphas)}
+df = pd.DataFrame(d)
+
+#plots distribution weighted error at each iteration
+print ggplot(df,aes(x='T', y='error')) + geom_point(color = "blue") \
+      + geom_line(color = "lightblue") + ggtitle("Distribution Weighted Error") + theme_matplotlib()
+
+#plots alphas value at each iteration
+print ggplot(df, aes(x = "T", y = "alpha")) + geom_point(color = "blue") + ggtitle("Alphas at Iteration T") \
+          + geom_line(color = "lightblue")  + theme_matplotlib()
+
+#plots the training error
+prediction_df = get_training_accuracy(pos_trainpath, neg_trainpath, (alphas, models, coords, features), T = None)
+
+prediction_df["pos_error"] = 1- prediction_df["pos_count"]/ prediction_df["pos_acc_count"]
+prediction_df["neg_error"] = 1- prediction_df["neg_count"]/ prediction_df["neg_acc_count"]
+
+#blue["positive_accuracy"] = blue["pos_count"]/ blue["pos_acc_count"]
+
+pos_df = prediction_df[["T","pos_error"]]
+pos_df["labels"] = "+"
+pos_df.rename(columns = {"pos_error":"error"}, inplace = True)
+
+neg_df = prediction_df[["T","neg_error"]]
+neg_df["labels"] = "-"
+neg_df.rename(columns = {"neg_error":"error"}, inplace = True)
+
+
+df_train = pos_df.append(neg_df)
+
+print ggplot(df_train,aes(x='T', y='error', color = "labels")) +geom_point() + geom_line() + ggtitle("training error per iteration")
+
+#plots the testing error
+prediction_df_test = get_training_accuracy(pos_testpath, neg_testpath, (alphas, models, coords, features), T = None)
+
+prediction_df_test["pos_error"] = 1- prediction_df_test["pos_count"]/ prediction_df_test["pos_acc_count"]
+prediction_df_test["neg_error"] = 1- prediction_df_test["neg_count"]/ prediction_df_test["neg_acc_count"]
+
+#blue["positive_accuracy"] = blue["pos_count"]/ blue["pos_acc_count"]
+
+pos_df = prediction_df_test[["T","pos_error"]]
+pos_df["labels"] = "+"
+pos_df.rename(columns = {"pos_error":"error"}, inplace = True)
+
+neg_df = prediction_df_test[["T","neg_error"]]
+neg_df["labels"] = "-"
+neg_df.rename(columns = {"neg_error":"error"}, inplace = True)
+
+
+df_test = pos_df.append(neg_df)
+
+print ggplot(df_test,aes(x='T', y='error', color = "labels")) +geom_point() + geom_line() + ggtitle("testing error per iteration")
